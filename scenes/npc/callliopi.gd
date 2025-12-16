e3e85a40b6fcf5e4e7185e0534ea5fded1bb2573
Fajar Rahmad Jaya <Fajar.Rahmad.Jaya.Profile@gmail.com>
2025-12-16 21:48:47 +0700

    Move dialogue resource

--- scenes/npc/callliopi.gd
+++ scenes/npc/callliopi.gd
@@ -5,13 +5,15 @@ extends Node2D
 @onready var sprite = $AnimatedSprite2D
 
 
-var resource = load("res://assets/dialogues/test.dialogue")
+# Define where the dialogue resource is
+var dialogue_resource = load("res://dialogue/resources/test.dialogue")
 
 
-# Called when the node enters the scene tree for the first time.
+# Call interaction manager and assign the command
 func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
 	
 	
+# Call dialogoe manager
 func _on_interact():
-	DialogueManager.show_dialogue_balloon(resource, "start")
+	DialogueManager.show_dialogue_balloon(dialogue_resource, "start")
