8253592d1451deec480962b99864a55813aa9499
Fajar Rahmad Jaya <Fajar.Rahmad.Jaya.Profile@gmail.com>
2025-12-17 17:25:04 +0700

    Change floor1 map traversal

--- scenes/floors/Floor1_Awakening.gd
+++ scenes/floors/Floor1_Awakening.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
-		$AscensionCircle.on_puzzle_solved()
+		SceneManager.change_scene('res://scenes/floors/Floor2_IllusionOfTruth.tscn')
