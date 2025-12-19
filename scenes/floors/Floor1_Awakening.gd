0b363bc21e674ca108fa3307dde31e68b8a5bb48
Fajar Rahmad Jaya <Fajar.Rahmad.Jaya.Profile@gmail.com>
2025-12-19 16:17:54 +0700

    Remove floor1 unnecessary script

--- scenes/floors/Floor1_Awakening.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):
-		SceneManager.change_scene('res://scenes/floors/Floor2_IllusionOfTruth.tscn')
